f50478a3b978ff1e1d406de029372744cb2fb6c4
Muhamad Farhan Haidar <mfarhan1306@gmail.com>
2025-05-16 21:39:15 +0700

    update streamlit_app.py

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -5,55 +5,56 @@ from scipy import stats
 import os
 
 def calculate_dynamic_pricing(df, group_cols):
-    """Calculates dynamic pricing based on demand and supply."""
     # Pastikan tanggal sudah dalam format date
-    df['order_date'] = pd.to_datetime(df['order_purchase_timestamp']).dt.date
+    #df['order_date'] = pd.to_datetime(df['order_purchase_timestamp']).dt.date
 
     # Grouping berdasarkan kolom yang ditentukan
     grouped = df.groupby(group_cols).agg(
         number_of_orders=('order_id', 'count'),
         number_of_sellers=('seller_id', 'nunique'),
-        historical_cost=('price', 'mean')
-    ).dropna().reset_index()
+        historical_cost=('price', 'mean')).dropna().reset_index()
 
     # Percentile untuk multiplier
     high_demand = np.percentile(grouped['number_of_orders'], 75)
     low_demand = np.percentile(grouped['number_of_orders'], 25)
 
-    high_supply = np.percentile(grouped['number_of_orders'], 75)
+    high_supply = np.percentile(grouped['number_of_sellers'], 75)
     low_supply = np.percentile(grouped['number_of_sellers'], 25)
 
     # Demand multiplier
     grouped['demand_multiplier'] = np.where(
         grouped['number_of_orders'] > high_demand,
         grouped['number_of_orders'] / high_demand,
-        grouped['number_of_orders'] / low_demand
-    )
+        grouped['number_of_orders'] / low_demand)
 
     # Supply multiplier
     grouped['supply_multiplier'] = np.where(
-        grouped['number_of_sellers'] > low_supply,
-        high_supply / grouped['number_of_sellers'],
-        low_supply / grouped['number_of_sellers']
-    )
+        grouped['number_of_sellers'] > high_supply, 
+        low_supply/ grouped['number_of_sellers'],
+        high_supply / grouped['number_of_sellers'])
 
     # Threshold
-    demand_threshold_low = 0.1
-    supply_threshold_high = 0.1
+    demand_threshold_low = 0.5
+    supply_threshold_high = 0.5
 
     # Adjusted price
     cb_mlp = (
         np.maximum(grouped['demand_multiplier'], demand_threshold_low) *
-        np.maximum(grouped['supply_multiplier'], supply_threshold_high)
-    )
-    # Batasi multiplier agar tidak lebih dari 1.05 (5% kenaikan maksimum - disesuaikan)
-    max_increase = 1.05
+        np.maximum(grouped['supply_multiplier'], supply_threshold_high))
+    
+    # Batasi multiplier agar tidak lebih dari 1.3 (30% kenaikan maksimum)
+    max_increase = 1.15
     final_multiplier = np.minimum(cb_mlp, max_increase)
 
     # Hitung harga akhir dengan batas atas
     grouped['adjusted_price'] = grouped['historical_cost'] * final_multiplier
 
     return grouped
+
+
+
+def get_dynamic_pricing(df, group_cols):
+    return calculate_dynamic_pricing(df, group_cols)
 
 def feature_engineering():
     """Performs feature engineering on the e-commerce transaction data."""
@@ -67,6 +68,11 @@ def get_dataframe():
     return feature_engineering()
 
 if __name__ == "__main__":
+    df = pd.read_csv("data/e-commerce_transaction.csv") # Baca DataFrame di sini
+    data = get_dynamic_pricing(df, ['product_id','product_category_name', 'customer_state'])
+    print("Dynamic Pricing Data:")
+    print(data.head())
+
     processed_df = feature_engineering()
-    print("Feature engineering completed. Processed DataFrame:")
+    print("\nFeature engineering completed. Processed DataFrame:")
     print(processed_df.head())
